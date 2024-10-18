d0ba77dfa62f10b50b1d3bb598a5cbf58aa4bf67
AndersToft20 <93765094+AndersToft20@users.noreply.github.com>
2024-10-18 12:58:38 +0200

    update fold in out icons

--- src/components/monitoringMenu.tsx
+++ src/components/monitoringMenu.tsx
@@ -15,7 +15,7 @@ export default function MonitoringMenu({
 
 	return (
 		<div className="flex flex-col h-full rounded-lg border-neural_3 border-2 bg-neutral_2 px-2">
-			<div className="flex flex-row justify-between items-center border-b-2 gap-4 p-2">
+			<div className={`flex flex-row justify-between items-center ${!isCollapsed && 'border-b-2'} gap-4 p-2`}>
 				<h1 className="text-xl font-bold">Monitoring overview</h1>
 				<p className="text-sm">
 					{monitoredVessels.length} ships
@@ -30,11 +30,11 @@ export default function MonitoringMenu({
 			}
 			<button onClick={() => setIsCollapsed(!isCollapsed)} className="w-full flex flex-cols items-center justify-center bottom-0">
 				{isCollapsed
-					? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down" viewBox="0 0 16 16">
-						<path d="M3.204 5h9.592L8 10.481zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659" />
-					</svg>
-					: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up" viewBox="0 0 16 16">
-						<path d="M3.204 11h9.592L8 5.519zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659" />
+					? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+				  </svg>
+					: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
+						<path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
 					</svg>
 				}
 			</button>
